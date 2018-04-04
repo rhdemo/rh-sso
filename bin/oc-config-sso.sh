35fe58fa0b62eac1999ff236bb1589d6e32cbe5a
stianst <stianst@gmail.com>
2018-04-04 15:24:55 +0200

    Use https for kcadmin

--- bin/oc-config-sso.sh
+++ bin/oc-config-sso.sh
@@ -3,8 +3,12 @@
 cd `dirname $0`/..
 . config
 
-CONFIG=$PWD/.keycloak/kcadmin.config
-SERVER=http://`oc get routes sso -o jsonpath='{.spec.host}'`/auth
+TMP=`mktemp -d`
+TMP_CONFIG=$TMP/kcadmin-config
+TMP_CERT=$TMP/cert
+TMP_KEYSTORE=$TMP/keystore
+
+SERVER=https://`oc get routes secure-sso -o jsonpath='{.spec.host}'`/auth
 
 POD=`oc get pods -l deployment=sso-1 -o jsonpath='{.items[0].metadata.name}'`
 
@@ -23,7 +27,16 @@ echo "Pod $POD is ready! Continue configuring sso pod $POD";
 sleep 5; # Rather sleep until route is finished
 
 # TODO Use https
-#bin/kcadm.sh config truststore --config $CONFIG --trustpass $CERT_PASS $PWD/.certs/truststore.jks
+
+# Create truststore with cert
+
+HOST=`oc get route secure-sso -o jsonpath='{.spec.host}'`
+
+openssl s_client --connect $HOST:443 </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > cert > $TMP_CERT
+
+keytool -import -noprompt -trustcacerts -alias $HOST -file $TMP_CERT -keystore $TMP_KEYSTORE -storepass $CERT_PASS
+
+bin/kcadm.sh config truststore --config $CONFIG --trustpass $CERT_PASS $TMP_KEYSTORE
 
 bin/kcadm.sh config credentials --config $CONFIG --server $SERVER --realm master --user admin --password $ADMIN_PASS
 
@@ -34,3 +47,5 @@ bin/kcadm.sh create realms --config $CONFIG -f realm-summit.json \
 
 bin/kcadm.sh update --config $CONFIG -r summit identity-provider/instances/google -s config.clientId=$GOOGLE_CLIENT_ID -s config.clientSecret=$GOOGLE_CLIENT_SECRET
 bin/kcadm.sh update --config $CONFIG -r summit identity-provider/instances/developers -s config.clientId=$DEVELOPERS_CLIENT_ID -s config.clientSecret=$DEVELOPERS_CLIENT_SECRET
+
+rm -rf $TMP
